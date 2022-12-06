bbaee9330868506971d5f817d90b79c324e2ef2f
Daniel Hill <daniel@gluo.nz>
2022-12-06 23:15:55 +1300

    fix doom

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    doom-emacs.url = "github:doomemacs/doomemacs/9d4d5b756a8598c4b5c842e9f1f33148af2af8fd";
+    #doom-emacs.url = "github:doomemacs/doomemacs/9d4d5b756a8598c4b5c842e9f1f33148af2af8fd";
     doom-emacs.flake = false;
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
-    nix-doom-emacs.inputs.doom-emacs.follows = "doom-emacs";
+    #nix-doom-emacs.inputs.doom-emacs.follows = "doom-emacs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
